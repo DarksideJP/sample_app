1311b1674806effa6eb9fb09a6d322533288d6ed
DarksideJP <darknebula1419@gmail.com>
2020-02-02 06:39:25 +0000

    [Start]3-7

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
 
 	get "todolists/:id" => "todolists#show", as: "todolist"
 	#.../todolists/1 や　.../todolists/3 に該当する
-	
+
+	get "todolists/:id_edit" => "todolists#edit",as "edit_todolist"
+
 end
 
