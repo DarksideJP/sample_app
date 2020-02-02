5930cca4fcb424023154e96d8411c534a6d2404d
DarksideJP <darknebula1419@gmail.com>
2020-02-02 06:16:41 +0000

    [Start]3-6

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -1,21 +1,32 @@
 class TodolistsController < ApplicationController
-  def new
+	def new
   	#viewへ渡すためのインスタンス変数に空のモデルオブジェクトを生成する。
   	@list = List.new
-  end
+	end
 
-  def create
+	def create
   	# ストロングパラメーター使用
   	list = List.new(list_params)
   	# DBへ保存する
   	list.save
   	# トップ画面へリダイレクト
   	redirect_to "/top"
-end
-private
+	end
 
-def list_params
+	def index
+	@lists = List.all
+	end
+
+	def show
+
+		@list = List.find(params[:id])
+
+	end
+
+	private
+
+	def list_params
 	params.require(:list).permit(:title, :body)
-  end
+	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
-  get 'todolists/new'
+	get 'todolists/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-get "top"=>"homes#top"
+	get "top"=>"homes#top"
 
-post "todolists" => "todolists#create"
+	post "todolists" => "todolists#create"
 
 
-get "todolists" => "todolist#index"
+	get "todolists" => "todolists#index"
+
+	get "todolists/:id" => "todolists#show", as: "todolist"
+	#.../todolists/1 や　.../todolists/3 に該当する
+	
 end
 
